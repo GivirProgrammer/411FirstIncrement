f21b969012e33f9c47c92543fe4c6173ab028e4d
fattoli <fatih.sayarlioglu@ug.bilkent.edu.tr>
2024-01-06 22:30:52 +0300

    login screen, sign up screen, home screen deneme 1

--- 411Increment/App.js
+++ 411Increment/App.js
@@ -2,18 +2,24 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import WorkoutHistory from './Components/Workouthistory';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Icon from 'react-native-ionicons';
+
+import { useState } from 'react';
+
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './Components/HomeScreen';
-
+import LoginScreen from './Components/LoginScreen';
 
 const Tab =createBottomTabNavigator();
 
 export default function App() {
+
+
+  
   return (
+    
     <NavigationContainer>
     <Tab.Navigator initialRouteName='Home'>
+      <Tab.Screen name="Login" component={LoginScreen} />
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Workout History" component={WorkoutHistory} />
     </Tab.Navigator>
